d413ab5cffd9854c9ba3313f4fc79dcbaf7a207a
LeoEchenique <leo.echeni@gmail.com>
2023-01-24 21:48:42 -0300

    REFACTOR: reorder routes and changes on controller based on new implementations

--- api/src/routes/contact/contact.js
+++ api/src/routes/contact/contact.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const { authenticateReq } = require("../../middlewares/auth");
+const User = require("../../models/User");
+router.get("/all", authenticateReq, async (req, res) => {
+  let user = await User.find({});
+
+  res.send(user);
+});
 
 module.exports = router;
